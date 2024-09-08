c11663b27859d273ab775e997a2e45746902b261
deewakar-k <dakshftw01@gmail.com>
2024-09-08 09:10:08 +0530

    some animation

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ window.addEventListener("load", () => {
   const heading = document.querySelector(".heading");
   const footer = document.querySelector(".footer");
   const qs = document.querySelector(".qs");
+  const right = document.querySelector(".right");
 
   form.classList.add("slide-up");
   footer.classList.add("slide-up");
   heading.classList.add("slide-right");
   qs.classList.add("slide-left");
+  right.classList.add("slide-right");
 });
